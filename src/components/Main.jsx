db8b534cb238c0aaecff1de1ef94a1cfb19189c5
BILAL BENYOUSSEF <bilalbeny735@gmail.com>
2024-01-10 19:18:23 +0100

    new feature

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,32 +16,37 @@ export default function Main() {
     console.log(planetsInfo)
 
     return (
-        <main>
-            <h1>Planets Info</h1>
-            {isError && <div>Error: {JSON.stringify(error)}</div>}
-            {isLoading && <div className="loading-spinner">
-                <span></span>
-                <b>Loading...</b>
-            </div>}
-            <section className="planets-box">
-                {!isLoading &&
-                    planetsInfo.map(planet => {
-                        return <div key={planet.id}>
-                            <p className="title">Name: {planet.name}</p>
-                            <img src={planet.imgSrc.img} alt={planet.imgSrc.imgDescription
-                            } />
-                            <p>Description: {planet.description}</p>
-                            <div>
-                                <p>Mass: {planet.basicDetails.mass}</p>
-                                <p>Mass: {planet.basicDetails.volume}</p>
+        <>
+            <main>
+                <h1>Planets Info</h1>
+                {isError && <div>Error: {JSON.stringify(error)}</div>}
+                {isLoading && <div className="loading-spinner">
+                    <span></span>
+                    <b>Loading...</b>
+                </div>}
+                <section className="planets-box">
+                    {!isLoading &&
+                        planetsInfo.map(planet => {
+                            return <div key={planet.id}>
+                                <p className="title"><span className="marked-text">Name:</span> {planet.name}</p>
+                                <img src={planet.imgSrc.img} alt={planet.imgSrc.imgDescription
+                                } />
+                                <p><span className="marked-text">Description:</span> {planet.description}</p>
+                                <div>
+                                    <p><span className="marked-text">Mass: </span> {planet.basicDetails.mass}</p>
+                                    <p><span className="marked-text">Volume:</span> {planet.basicDetails.volume}</p>
+                                </div>
+                                <p>You can find more info on <a href={planet.wikiLink} title={planet.source} target="_blank" rel="noreferrer">here</a></p>
+
                             </div>
-                            <p>You can find more info on <a href={planet.wikiLink} title={planet.source} target="_blank" rel="noreferrer">here</a></p>
+                        })
+                    }
+                </section>
 
-                        </div>
-                    })
-                }
-            </section>
-
-        </main>
+            </main>
+            <footer>
+                <p>all rights are reserved {new Date().getFullYear()}</p>
+            </footer>
+        </>
     );
 }
